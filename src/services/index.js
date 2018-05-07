78380c51f2e8d4782272bdaf163918177cf05327
Ilya Fadeev <fadeev.ilya@gmail.com>
2018-05-07 00:47:51 -0400

    added questions and answers services

--- src/services/index.js
+++ src/services/index.js
@@ -7,7 +7,11 @@ const forgotPassword = require('./forgot-password/forgot-password.service.js')
 
 const kyc = require('./kyc/kyc.service.js')
 
-const icoBalance = require('./ico-balance/ico-balance.service.js');
+const icoBalance = require('./ico-balance/ico-balance.service.js')
+
+const answers = require('./answers/answers.service.js')
+
+const questions = require('./questions/questions.service.js')
 
 module.exports = function () {
   const app = this
@@ -15,5 +19,7 @@ module.exports = function () {
   app.configure(postmark)
   app.configure(forgotPassword)
   app.configure(kyc)
-  app.configure(icoBalance);
+  app.configure(icoBalance)
+  app.configure(answers)
+  app.configure(questions)
 }
